b4af0ac4e5068554395a791733fad1d9e4b349ff
nzmpi <nizami.abdulov@gmail.com>
2023-10-21 17:29:41 +0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Custom encryption
+
+Encrypt and decrypt any text
+
+## How to use
+
+ 1. `yarn add hardhat`
+
+ 2. Change `customPrefix` to a desired prefix
+
+ 3. Change `seed` to any string you want
+
+ 4. `node customAddress.js`
+
---
+++ encryptAndDecrypt.js
@@ -0,0 +1,72 @@
+const { 
+  keccak256,
+  toUtf8Bytes,
+} = require("ethers/lib/utils");
+const { v4: uuidv4 } = require('uuid');
+var seedrandom = require('seedrandom');
+require('dotenv').config();
+
+const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:'\",.<>?/\\`~";
+
+const stringToHex = (str) => {
+  let hex = '';
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    const hexValue = charCode.toString(16);
+    hex += hexValue.padStart(2, '0');
+  }
+  return '0x' + hex;
+};
+
+const hexToString = (hex) => {
+  hex = hex.slice(2); // remove 0x
+  let str = '';
+  for (let i = 0; i < hex.length; i += 2) {
+    const hexValue = hex.substr(i, 2);
+    const decimalValue = parseInt(hexValue, 16);
+    str += String.fromCharCode(decimalValue);
+  }
+  return str;
+};
+
+const addGarbage = (text) => {
+  text = text + " ~~garbage: "
+  let characters = keccak256(toUtf8Bytes(uuidv4())).slice(2); // remove 0x
+  let character;
+  while (text.length < 400) {
+    character = characters[0];
+    characters = characters.slice(1);
+    text += character;
+    if (characters.length == 0) {
+      characters = keccak256(toUtf8Bytes(uuidv4())).slice(2);
+    }
+  }
+  return text;
+}
+
+const encrypt = (text, secret) => {
+  text = addGarbage(text);
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    var rng = seedrandom(keccak256(toUtf8Bytes(secret)) + i);
+    result += String.fromCharCode(text.charCodeAt(i) ^ symbols.charCodeAt(Math.floor(rng()*1000) % symbols.length));
+  }
+  return stringToHex(result);
+}
+
+const decrypt = (hex, secret) => {
+  const str = hexToString(hex);
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    var rng = seedrandom(keccak256(toUtf8Bytes(secret)) + i);
+    result += String.fromCharCode(str.charCodeAt(i) ^ symbols.charCodeAt(Math.floor(rng()*1000) % symbols.length));
+  }
+  return result;
+}
+
+const secret = process.env.SECRET;
+const text_to_encrypt = process.env.TEXT_TO_ENCRYPT;
+console.log("Encrypted Text = ", encrypt(text_to_encrypt, secret));
+
+const text_to_decrypt = process.env.TEXT_TO_DECRYPT;
+console.log("Decrypted Text = ", decrypt(text_to_decrypt, secret));
